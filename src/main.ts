2e578caeed837227f35a4300e7a05d9361525711
unknown <faizaltriasaa.gmail.com>
2025-11-30 17:29:31 +0700

    fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  
   console.log('--------------------------------------');
   console.log('CEK DEPLOY: VERSI CORS BARU (ORIGIN: TRUE)');
   console.log('--------------------------------------');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    },
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  // -------------------------
-
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
