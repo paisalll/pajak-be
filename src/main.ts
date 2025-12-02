d78c14c7d3f2b04ac2783a09307ce26557c708c2
unknown <faizaltriasaa.gmail.com>
2025-12-02 16:53:58 +0700

    fix: parse bigInt

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
