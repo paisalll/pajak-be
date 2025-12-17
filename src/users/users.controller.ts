35dd247f978a0cf124caa27e9aa0948f016415de
unknown <faizaltriasaa.gmail.com>
2025-12-17 17:46:30 +0700

    feat: bycrypt password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 
-@UseGuards(AuthGuard('jwt')) // Semua endpoint user butuh Login
+@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
